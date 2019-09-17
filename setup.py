ad885ebe4a13b989346b50ff853e42e0b4b73944
Gjert Ingar Gjersund <gjert@gjersund.com>
2019-09-17 23:29:57 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,11 @@ setup(
     package_dir={'':'src'},
     packages=find_packages('src'),
     scripts=['src/manage.py'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: MIT License",
+    ],
+    python_requires='>=3.7',
+    install_requires=['Django>=2.2'],
     url='https://github.com/ggjersund/personal-website'
 )
