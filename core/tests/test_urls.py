7f3428eef8c9d428162a8c919455f4bf5027ce9f
Gjert Ingar Gjersund <gjert@gjersund.com>
2019-09-18 13:08:32 +0200

    Update travis to include linting

--- core/tests/test_urls.py
+++ core/tests/test_urls.py
@@ -6,8 +6,5 @@ from django.urls import reverse, resolve
 
 
 def test_index_url():
-    """
-    Test if index url is index
-    """
     path = reverse('index')
     assert resolve(path).view_name == 'index'
