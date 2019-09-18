6f56b66ea4f4eda63439fb90293add3a847b178a
Gjert Ingar Gjersund <gjert@gjersund.com>
2019-09-18 13:12:48 +0200

    Fix linting problem

--- core/tests/test_urls.py
+++ core/tests/test_urls.py
@@ -6,5 +6,8 @@ from django.urls import reverse, resolve
 
 
 def test_index_url():
+    """
+    Test if index url is index
+    """
     path = reverse('index')
     assert resolve(path).view_name == 'index'
